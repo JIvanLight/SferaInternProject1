ca2a5cf72b2dada1f76587b414fb4c9cc59f3ebd
Ivan Zharkov <qwer_steel@mail.ru>
2022-10-30 18:33:52 +0300

    Fixed bugs

--- app/src/main/java/ru/jivan/sferainternproject1/MainActivity.kt
+++ app/src/main/java/ru/jivan/sferainternproject1/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.jivan.sferainternproject1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.jivan.sferainternproject1.databinding.FragmentMainProfileBinding
 
 class MainActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = FragmentMainProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
